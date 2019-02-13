b60f696a6dff79f4c6eec47f0418a84f1ad17817
James <Icedshot@gmail.com>
2019-02-13 03:33:33 +0000

    thread fun

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <imgui/misc/freetype/imgui_freetype.h>
 #include "ship_components.hpp"
 #include "entity.hpp"
+#include <windows.h>
 
 template<sf::Keyboard::Key k, int n, int c>
 bool once()
@@ -51,6 +52,111 @@ bool once()
 }
 
 #define ONCE_MACRO(x) once<x, __LINE__, __COUNTER__>()
+
+void server_thread()
+{
+    entity_manager entities;
+
+    ship* test_ship = entities.make_new<ship>();
+
+    component thruster, warp, shields, laser, sensor, comms, armour, ls, coolant, power_generator, crew;
+
+    thruster.add(component_info::POWER, -1);
+    thruster.add(component_info::THRUST, 1);
+    thruster.add(component_info::HP, 0, 1);
+
+    warp.add(component_info::POWER, -1);
+    warp.add(component_info::WARP, 0.5, 10);
+    warp.add(component_info::WARP, -0.1);
+    warp.add(component_info::WARP, 0.1);
+    warp.add(component_info::HP, 0, 5);
+
+    shields.add(component_info::SHIELDS, 0.5, 10);
+    shields.add(component_info::POWER, -1);
+    shields.add(component_info::HP, 0, 5);
+
+    laser.add(component_info::POWER, -1);
+    laser.add(component_info::WEAPONS, 1);
+    laser.add(component_info::HP, 0, 2);
+    laser.add(component_info::CAPACITOR, 1, 20);
+
+    laser.add_on_use(component_info::CAPACITOR, -10);
+
+    //laser.info[3].held = 0;
+
+    sensor.add(component_info::POWER, -1);
+    sensor.add(component_info::SENSORS, 1);
+    sensor.add(component_info::HP, 0, 1);
+
+    comms.add(component_info::POWER, -0.5);
+    comms.add(component_info::COMMS, 1);
+    comms.add(component_info::HP, 0, 1);
+
+    /*sysrepair.add(component_info::POWER, -1);
+    sysrepair.add(component_info::SYSTEM, 1);
+    sysrepair.add(component_info::HP, 0.1, 1);*/
+
+    armour.add(component_info::ARMOUR, 0.1, 10);
+    armour.add(component_info::POWER, -0.5);
+    armour.add(component_info::HP, 0, 10);
+
+    ls.add(component_info::POWER, -1);
+    ls.add(component_info::LIFE_SUPPORT, 1, 5);
+    ls.add(component_info::HP, 0.01, 5);
+
+    coolant.add(component_info::COOLANT, 1, 20);
+    coolant.add(component_info::HP, 0, 1);
+
+    power_generator.add(component_info::POWER, 10, 50);
+    power_generator.add(component_info::HP, 0, 10);
+
+    //power_generator.info[1].held = 0;
+    //power_generator.info[0].held = 0;
+
+    ///need to add the ability for systems to start depleting if they have insufficient
+    ///consumption
+    ///or... maybe we just cheat and add a crew death component that's offset by crew replenishment?
+    //crew.add(component_info::POWER, 1, 5);
+    crew.add(component_info::HP, 0.1, 10);
+    crew.add(component_info::LIFE_SUPPORT, -0.5, 1);
+    crew.add(component_info::COMMS, 0.1);
+    crew.add(component_info::CREW, 0.01, 100);
+    crew.add(component_info::CREW, -0.01); ///passive death on no o2
+
+
+    test_ship->add(thruster);
+    test_ship->add(warp);
+    test_ship->add(shields);
+    test_ship->add(laser);
+    test_ship->add(sensor);
+    test_ship->add(comms);
+    //test_ship.add(sysrepair);
+    test_ship->add(armour);
+    test_ship->add(ls);
+    test_ship->add(coolant);
+    test_ship->add(power_generator);
+    test_ship->add(crew);
+
+    test_ship->position = {400, 400};
+
+    ship* test_ship2 = entities.make_new<ship>(*test_ship);
+
+    test_ship2->position = {600, 400};
+
+    double frametime_dt = 1;
+
+    sf::Clock clk;
+
+    while(1)
+    {
+        entities.tick(frametime_dt);
+        entities.cleanup();
+
+        frametime_dt = (clk.restart().asMicroseconds() / 1000.) / 1000.;
+
+        Sleep(10);
+    }
+}
 
 int main()
 {
