0d0aeadf3c249f1d6e43bc47d7d5cb84b33ca894
James <Icedshot@gmail.com>
2019-05-28 02:07:18 +0100

    beginning of cpu execution

--- script_execution.cpp
+++ script_execution.cpp
@@ -1,1 +1,73 @@
 #include "script_execution.hpp"
+
+void register_value::make(const std::string& str)
+{
+    if(str.size() == 0)
+        throw std::runtime_error("Bad register value, 0 length");
+
+    if(str.size() == 1)
+    {
+        if(str[0] == 'G' || str[0] == 'X')
+        {
+            set_reg(registers::GENERAL_PURPOSE0);
+        }
+        else if(str[0] == 'T')
+        {
+            set_reg(registers::TEST);
+        }
+        else
+        {
+            throw std::runtime_error("Bad register " + str);
+        }
+    }
+
+    if(str.size() == 2)
+    {
+        if(str[0] == 'X' || str[0] == 'G')
+        {
+            int num = str[1] - '0';
+
+            if(num == 0)
+            {
+                set_reg(registers::GENERAL_PURPOSE0);
+            }
+            else
+            {
+                throw std::runtime_error("No G/X registers available > 0");
+            }
+        }
+    }
+
+    if(str.size() >= 2)
+    {
+        if(str.front() == '\"' && str.back() == '\"')
+        {
+            std::string stripped = str;
+            stripped.erase(stripped.begin());
+            stripped.pop_back();
+
+            set_symbol(stripped);
+            return;
+        }
+
+        if(str.front() == '\"' && str.back() != '\"')
+            throw std::runtime_error("String not terminated");
+
+        set_label(str);
+    }
+}
+
+void cpu_state::step()
+{
+
+}
+
+void cpu_state::debug_state()
+{
+
+}
+
+void cpu_tests()
+{
+    cpu_state test;
+}
--- script_execution.hpp
+++ script_execution.hpp
@@ -1,6 +1,143 @@
 #ifndef SCRIPT_EXECUTION_HPP_INCLUDED
 #define SCRIPT_EXECUTION_HPP_INCLUDED
 
+#include <string>
+#include <vector>
+#include <map>
 
+namespace registers
+{
+    enum type
+    {
+        TEST,
+        GENERAL_PURPOSE0,
+        COUNT,
+    };
+
+    static inline std::vector<std::string> rnames
+    {
+        "T",
+        "X0",
+    };
+}
+
+struct register_value
+{
+    registers::type reg = registers::COUNT;
+    int value = 0;
+    std::string symbol;
+    std::string label;
+
+    int which = 0;
+
+    void make(const std::string& str);
+
+    bool is_reg()
+    {
+        return which == 0;
+    }
+
+    bool is_int()
+    {
+        return which == 1;
+    }
+
+    bool is_symbol()
+    {
+        return which == 2;
+    }
+
+    bool is_label()
+    {
+        return which == 3;
+    }
+
+    void set_reg(registers::type type)
+    {
+        reg = type;
+        which = 0;
+    }
+
+    void set_int(int v)
+    {
+        value = v;
+        which = 1;
+    }
+
+    void set_symbol(const std::string& symb)
+    {
+        symbol = symb;
+        which = 2;
+    }
+
+    void set_label(const std::string& lab)
+    {
+        label = lab;
+        which = 3;
+    }
+};
+
+///so extensions to exapunks syntax
+///[0xFF] or [reg] means memory address?
+///basically: need some way to store data, need some way to poke puzzles and obscure hardware
+namespace instructions
+{
+    enum type
+    {
+        COPY,
+        ADDI,
+        SUBI,
+        MULI,
+        DIVI,
+        MODI,
+        SWIZ,
+        MARK,
+        JUMP,
+        TJMP,
+        FJMP,
+        TEST,
+        //REPL,
+        HALT,
+        KILL,
+        LINK,
+        HOST,
+        MODE,
+        VOID,
+        MAKE,
+        GRAB,
+        FILE,
+        SEEK,
+        DROP,
+        WIPE,
+        NOOP,
+        NOTE,
+        RAND,
+        AT_REP,
+        AT_END,
+        AT_N_M,
+        //WAIT,
+        DATA,
+        COUNT,
+    };
+}
+
+struct instruction
+{
+    instructions::type type = instructions::COUNT;
+    std::vector<register_value> args;
+};
+
+struct cpu_state
+{
+    std::map<registers::type, register_value> register_states;
+    std::vector<instruction> inst;
+    int pc = 0; ///instruction to be executed next
+
+    void step();
+
+    void debug_state();
+};
+
+void cpu_tests();
 
 #endif // SCRIPT_EXECUTION_HPP_INCLUDED
